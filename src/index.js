1899120dffffc82b460d95f38ad4845b1f4f643b
Zackary Maupin <zmaupin@gmail.com>
2020-03-18 13:34:57 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let booksList = [
+    {"title": "Hunger", "author": "Roxane Gay", "pages": 320},
+    {"title": "The Sun Also Rises", "author": "Ernset Hemingway", "pages": 260},
+    {"title": "White Teeth", "author": "Zadie Smith", "pages": 480},
+    {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Book = ({title, author, pages, freeBookmark}) => {
+    return (
+        <section>
+          <h2>{title}</h2>
+          <p>by: {author}</p>
+          <p>Pages: {pages} pages</p>
+          <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
+        </section>
+    )
+}
+
+const Hiring = () =>
+    <div>
+        <p>The library is hiring. Go to www.library.com/jobs for more.</p>
+    </div>
+
+const NotHiring = () =>
+    <div>
+        <p>The library is not hiring. Check back later for more info.</p>
+    </div>
+ 
+class Library extends React.Component {
+
+    state = {
+        open: false,
+        freeBookmark: true
+    }
+
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open
+        }))
+    }
+
+
+    render() {
+        const { books } = this.props
+
+        return (
+            <div>
+                {this.state.hiring ? <Hiring /> : <NotHiring />}
+                <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
+                {books.map(
+                    (book, i) =>
+                    <Book
+                        key={i}
+                        title={book.title}
+                        author={book.author}
+                        pages={book.pages}
+                        freeBookmark={this.state.freeBookmark}/>
+
+                )}
+            </div>
+        )
+    }
+}
+
+render(
+    <Library books={booksList} />,
+    document.getElementById('root')
+)
