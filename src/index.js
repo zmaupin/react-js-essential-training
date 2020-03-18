7b5f85df12bc070b44cf7a322d7504a3963f4591
Zackary Maupin <zmaupin@gmail.com>
2020-03-18 14:23:55 -0400

    done with react-js-essential-training learning modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Library from './library.js';
 
 let booksList = [
     {"title": "Hunger", "author": "Roxane Gay", "pages": 320},
@@ -7,64 +8,6 @@ let booksList = [
     {"title": "White Teeth", "author": "Zadie Smith", "pages": 480},
     {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
 ]
-
-const Book = ({title, author, pages, freeBookmark}) => {
-    return (
-        <section>
-          <h2>{title}</h2>
-          <p>by: {author}</p>
-          <p>Pages: {pages} pages</p>
-          <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
-        </section>
-    )
-}
-
-const Hiring = () =>
-    <div>
-        <p>The library is hiring. Go to www.library.com/jobs for more.</p>
-    </div>
-
-const NotHiring = () =>
-    <div>
-        <p>The library is not hiring. Check back later for more info.</p>
-    </div>
- 
-class Library extends React.Component {
-
-    state = {
-        open: false,
-        freeBookmark: true
-    }
-
-    toggleOpenClosed = () => {
-        this.setState(prevState => ({
-            open: !prevState.open
-        }))
-    }
-
-
-    render() {
-        const { books } = this.props
-
-        return (
-            <div>
-                {this.state.hiring ? <Hiring /> : <NotHiring />}
-                <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
-                <button onClick={this.toggleOpenClosed}>Change</button>
-                {books.map(
-                    (book, i) =>
-                    <Book
-                        key={i}
-                        title={book.title}
-                        author={book.author}
-                        pages={book.pages}
-                        freeBookmark={this.state.freeBookmark}/>
-
-                )}
-            </div>
-        )
-    }
-}
 
 render(
     <Library books={booksList} />,
